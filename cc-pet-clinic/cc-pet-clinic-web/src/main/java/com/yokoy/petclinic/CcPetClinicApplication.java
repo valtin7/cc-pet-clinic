49dfc4d1c110d9d90da5950fdae3530b1f108c6c
yokoyama <sunagay@green.email.ne.jp>
2019-04-07 01:30:33 +0900

    refactoring to multimodule maven project. closes #1

---
+++ cc-pet-clinic/cc-pet-clinic-web/src/main/java/com/yokoy/petclinic/CcPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.yokoy.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcPetClinicApplication.class, args);
+	}
+
+}
